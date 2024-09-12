11c3c3197ea2a57e16363bec11cd51f79e9fc6b0
philippekurz <philippekurz@gmail.com>
2024-09-12 17:19:49 +0200

    Choix aléatoire de l'url

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redirect;
 
 Route::get('/', function () {
-    return view('welcome');
+    $index = rand(1,6);
+    $urls = ['https://adlen.ifcsioslam.fr',
+            'https://dilan.ifcsioslam.fr',
+            'https://maxime.ifcsioslam.fr',
+            'https://oceane.ifcsioslam.fr',
+            'https://nicolas.ifcsioslam.fr',
+            'https://loise.ifcsioslam.fr'];
+            
+    
+    $url=$urls[$index];
+    return Redirect::to($url);
 });
